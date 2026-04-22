e2e9211488bd5857edb57578469fc07f6245f14d
jswiet <jus.swie.2@gmail.com>
2026-04-22 15:20:59 +0200

    Enable RabbitMq with @EnableRabbit annotation

--- src/main/java/com/project/mylittleshop/MyLittleShopApplication.java
+++ src/main/java/com/project/mylittleshop/MyLittleShopApplication.java
@@ -1,11 +1,13 @@
 package com.project.mylittleshop;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRabbit
 public class MyLittleShopApplication {
     
     public static void main(String[] args) {
