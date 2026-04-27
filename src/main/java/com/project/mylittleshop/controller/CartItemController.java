89c0962e344049f6314bca21a66cc2d9f84e2845
jswiet <jus.swie.2@gmail.com>
2026-04-27 18:27:01 +0200

    Add admin endpoint

--- src/main/java/com/project/mylittleshop/controller/CartItemController.java
+++ src/main/java/com/project/mylittleshop/controller/CartItemController.java
@@ -6,6 +6,7 @@ import com.project.mylittleshop.service.CartItemService;
 import jakarta.validation.Valid;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,6 +26,7 @@ public class CartItemController {
     }
     
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public List<CartItemDTO> getAllCarts() {
         return cartItemService.getAllCarts();
     }
